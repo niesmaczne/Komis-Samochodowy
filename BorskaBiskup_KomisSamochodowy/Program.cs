f655e15faab07bb278a1683ac4361de3ffe326bb
Nikola Borska <nikola.a.borska@gmail.com>
2022-01-21 19:13:02 +0100

    created C# project

---
+++ BorskaBiskup_KomisSamochodowy/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BorskaBiskup_KomisSamochodowy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
